88897b0163d05b11631c11b5f8798a93d471632d
RolandCrow <Yevgeniy_Yeltsov@mail.ru>
2025-03-26 20:02:56 +0300

    Initial commit

---
+++ src/patterns/Singleton.kt
@@ -0,0 +1,15 @@
+package patterns
+
+fun main() {
+    Logger.log("Hello World")
+}
+
+object Logger {
+    init {
+        println("I was accessed for the first time")
+    }
+
+    fun log(message: String){
+        println("Logging $message")
+    }
+}
