d0fee46b7482cc4fdca80a9e1ca98c553e25e17a
RolandCrow <Yevgeniy_Yeltsov@mail.ru>
2025-04-10 21:18:33 +0300

    Добавил поведенческие паттерны - итератор и состояние

---
+++ src/behavioral_patterns/State.kt
@@ -0,0 +1,63 @@
+package behavioral_patterns
+
+fun main() {
+    val snail = Snail()
+    snail.seeHero()
+    snail.getHit(1)
+    snail.getHit(10)
+}
+
+interface WhatCanHappen {
+    fun seeHero()
+    fun getHit(pointsOfDamage: Int)
+    fun calmAgain()
+}
+
+class Snail: WhatCanHappen {
+    private var healthPoints = 10
+    private var mood: Mood = Still
+
+    override fun seeHero() {
+        mood = when(mood) {
+            is Still -> {
+                println("Aggressive")
+                Aggressive
+            }
+            else -> {
+                println("No change")
+                mood
+            }
+        }
+    }
+
+    override fun getHit(pointsOfDamage: Int) {
+        println("Hit for $pointsOfDamage points")
+        healthPoints -= pointsOfDamage
+        println("Health: $healthPoints")
+
+        mood = when {
+            (healthPoints <= 0) -> {
+                println("Dead")
+                Dead
+            }
+            mood is Aggressive -> {
+                println("Retreating")
+                Retreating
+            }
+            else -> {
+                println("No change")
+                mood
+            }
+        }
+    }
+
+    override fun calmAgain() {}
+
+}
+
+sealed class Mood {}
+
+data object Still: Mood()
+data object Aggressive:Mood()
+data object Retreating: Mood()
+data object Dead:Mood()
